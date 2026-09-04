e225a96a68b87aa0cef6b52f236fcb8c6992a6aa
yuktha <yu.asr7216@gmail.com>
2026-09-04 22:52:56 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,11 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://revenue-recovery-agent-um8o.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
